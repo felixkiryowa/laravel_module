743acc7fd17c504b3afbea9d556affdf3498ce97
francis kiryowa <franciskiryowa68@gmail.com>
2018-07-21 10:51:52 -0700

    Configured The JWT API and created The AuthController

--- config/auth.php
+++ config/auth.php
@@ -13,8 +13,12 @@ return [
     |
     */
 
+    // 'defaults' => [
+    //     'guard' => 'web',
+    //     'passwords' => 'users',
+    // ],
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -35,14 +39,26 @@ return [
     |
     */
 
-    'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
+    // 'guards' => [
+    //     'web' => [
+    //         'driver' => 'session',
+    //         'provider' => 'users',
+    //     ],
 
+    //     'api' => [
+    //         'driver' => 'token',
+    //         'provider' => 'users',
+    //     ],
+    // ],
+
+    'defaults' => [
+    'guard' => 'api',
+    'passwords' => 'users',
+            ],
+
+    'guards' => [
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
